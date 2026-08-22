55d779afcb5a24edb0d9084a12b9f6f995f75407
swopnil676 <swopnilbiswas437@gmail.com>
2026-08-23 00:46:30 +0530

    dataTypes

---
+++ 01_Basics/2_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as a newer version
+
+// alert(3+3) // we are using nodejs , not browser
+
+console.log(3+3); // code readability should be high
+console.log("Swopnil");
+
+
+
+let name = "sam"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
